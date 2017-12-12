f1954bb64aabf229c6c32a837e8801d0887ccdcd
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-12-13 00:25:01 +0900

    validate version

--- src/http/ngx_http_mruby_module.c
+++ src/http/ngx_http_mruby_module.c
@@ -83,8 +83,10 @@ static char *ngx_http_mruby_exit_worker_inline(ngx_conf_t *cf, ngx_command_t *cm
 #if (NGX_HTTP_SSL)
 static char *ngx_http_mruby_ssl_handshake_phase(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static char *ngx_http_mruby_ssl_handshake_inline(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+#if OPENSSL_VERSION_NUMBER >= 0x10101000L
 static char *ngx_http_mruby_ssl_client_hello_phase(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static char *ngx_http_mruby_ssl_client_hello_inline(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+#endif
 #endif /* NGX_HTTP_SSL */
 static char *ngx_http_mruby_server_config_inline(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
